6fad66d378186bec682c17a4179a6b8a48291a18
Aldo Marcelino <aldo115marcelino@gmail.com>
2024-01-01 17:47:41 +0700

    initialize model

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # chatbot-mk-I
+
 Using NLP and NLTK
+
+![Binaryhood](Logo/ramworks_logo.png)
+
+# ChatBot
+
+## Installation & Setup
+
+[Install Python] https://www.dataquest.io/blog/installing-python-on-mac/
+
+[Install pip] https://phoenixnap.com/kb/install-pip-mac
+
+If you have Python & pip installed then check their version in the terminal or command line tools
+
+```
+python3 --version
+```
+
+```
+pip --version
+```
+
+## Installing Flask
+
+In your terminal run the requirements.txt file using this pip
+
+```
+pip install -r requirements.txt
+```
+
+## Running ChatBot Application in Terminal
+
+```
+cd into your directory
+```
+
+```
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,92 @@
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+# import torch
+
+from flask import Flask, render_template, request
+# from transformers import AutoModelForCausalLM, AutoTokenizer
+from nltk.stem import WordNetLemmatizer
+from keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.h5')
+
+
+# tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+# model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    ints = predict_class (msg)
+    res = get_response (ints, intents)
+    return res
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words (sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class (sentence):
+    bow = bag_of_words (sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes [r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice (i['responses'])
+            break
+    return result
+
+# def get_Chat_response(text):
+
+#     # Let's chat for 5 lines
+#     for step in range(5):
+#         # encode the new user input, add the eos_token and return a tensor in Pytorch
+#         new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+
+#         # append the new user input tokens to the chat history
+#         bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+
+#         # generated a response while limiting the total chat history to 1000 tokens, 
+#         chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+
+#         # pretty print last ouput tokens from bot
+#         return "SUka suka gw lah yah"
+
+
+if __name__ == '__main__':
+    app.run()
